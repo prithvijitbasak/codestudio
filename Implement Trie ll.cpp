434fe4d8c55fc5cd09621c28ef5bbb1ee41fc75e
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-14 14:45:40 +0530

    Update Implement Trie ll.cpp

--- Implement Trie ll.cpp
+++ Implement Trie ll.cpp
@@ -1,6 +1,6 @@
 //Editorial: https://youtu.be/K5pcpkEMCN0
 //  This problem has a pre-requisite problem that must be solved 
-//and it is present in LeetCode(Implement Trie (Prefix Tree)) as well as in codestudio alse. 
+//and it is present in LeetCode(Implement Trie (Prefix Tree)) as well as in codestudio also. 
 //The solution of that problem is available on my LeetCode repository.
 struct Node{
     Node* links[26];
